72bb6a5cdb7a1fc85bf99982547a291b50f8b6bb
Asritp5 <154317067+Asritp5@users.noreply.github.com>
2023-12-25 15:43:45 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import mysql.connector
+
+
+cursor = None  #prevent cursor not defined error
+
+#global variable declaration
+global df_HR
+df_HR=pd.DataFrame()
+global df_USER
+df_USER=pd.DataFrame()
+
+
+
+# Establish a connection to database
+try:
+    conn = mysql.connector.connect(
+    host=st.secrets["host"],
+    user=st.secrets["user"],
+    password=st.secrets["password"],
+    database=st.secrets["database"])
+    cursor = conn.cursor() # Create a cursor that interacts with db
+
+except Exception as e:
+    st.error(f"Sorry ," + e.__class__.__name__ + " has occurred" )
+    if conn in locals() and conn.is_connected():
+        conn.close()
+    if cursor in locals() and cursor is not None:
+        cursor.close()    
+
+
+def match(ID):
+    try:
+        global df_HR , df_USER
+        # Execute  query with  placeholder for  ID
+        cursor.execute("SELECT * FROM HR_REQUIREMENTS WHERE ADMIN_ID=%s", (ID,)) #(ID,) creates one element tuple
+        # Convert the result to a DataFrame 
+        df_HR = create_df()
+
+        st.title('HR REQ')
+
+        # Display data 
+        st.write(df_HR)
+
+    except Exception as e:
+        
+        st.error(f"Error occurred: {e}")
+        
+    finally:
+        #close the connection
+        if conn:
+         conn.close()
+         st.success("DB connection closed") 
+
+#function for creating dataframes
+def create_df():
+  #Fetch data
+  result = cursor.fetchall()
+  #Convert the result to a DataFrame 
+  df= pd.DataFrame(result, columns=[col[0] for col in cursor.description])
+  return df
+
+match('101')         
